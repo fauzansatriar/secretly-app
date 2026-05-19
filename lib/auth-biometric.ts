8b365ee24ff0afa1bfd2f20507274ee34223d0ca
Fauzan Satria <fauzansatriar20@gmail.com>
2026-05-19 16:54:24 +0700

    fix: cast challenge to buffersource

--- lib/auth-biometric.ts
+++ lib/auth-biometric.ts
@@ -79,7 +79,7 @@ export async function registerBiometric(): Promise<boolean> {
         name: "user@secretly.app",
         displayName: "Secretly User",
       },
-      challenge: new TextEncoder().encode(challenge as string),
+      challenge: challenge as unknown as BufferSource,
       pubKeyCredParams: [
         { alg: -7, type: "public-key" },   // ES256
         { alg: -257, type: "public-key" }, // RS256
